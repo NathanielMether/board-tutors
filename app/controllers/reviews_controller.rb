d16a0ae869ddbf551800bf6cb1210c6f359641c5
NathanielMether <nmet3@student.monash.edu>
2017-11-07 15:54:49 +1100

    Added the user journey for adding reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,11 @@
 class ReviewsController < ApplicationController
+  before_action :set_profile
   before_action :set_review, only: [:show, :edit, :update, :destroy]
 
   # GET /reviews
   # GET /reviews.json
   def index
-    @reviews = Review.all
+    @reviews = Review.where("profile_id = #{@profile.id}")
   end
 
   # GET /reviews/1
@@ -25,11 +26,13 @@ class ReviewsController < ApplicationController
   # POST /reviews.json
   def create
     @review = Review.new(review_params)
+    @review.user = current_user
+    @review.profile = @profile
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
-        format.json { render :show, status: :created, location: @review }
+        format.html { redirect_to profile_reviews_path, notice: 'Review was successfully created.' }
+        format.json { render :index, status: :created, location: @review }
       else
         format.html { render :new }
         format.json { render json: @review.errors, status: :unprocessable_entity }
@@ -42,8 +45,8 @@ class ReviewsController < ApplicationController
   def update
     respond_to do |format|
       if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
-        format.json { render :show, status: :ok, location: @review }
+        format.html { redirect_to profile_reviews_path, notice: 'Review was successfully updated.' }
+        format.json { render :index, status: :ok, location: @review }
       else
         format.html { render :edit }
         format.json { render json: @review.errors, status: :unprocessable_entity }
@@ -56,12 +59,15 @@ class ReviewsController < ApplicationController
   def destroy
     @review.destroy
     respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
+      format.html { redirect_to profile_reviews_url, notice: 'Review was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
+    def set_profile
+      @profile = Profile.find(params[:profile_id])
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_review
       @review = Review.find(params[:id])
@@ -69,6 +75,6 @@ class ReviewsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
-      params.require(:review).permit(:user_id, :profile_id, :content, :rating)
+      params.require(:review).permit(:content, :rating)
     end
 end
