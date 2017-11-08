d42a7ee5c627df8e425974a9cd6efc7801c6f0b1
NathanielMether <nmet3@student.monash.edu>
2017-11-08 15:37:50 +1100

    Added teacher and student sections to contacts, added profile picture to contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,15 @@
 class ContactsController < ApplicationController
   def index
+    # Get the relevant users
+    as_user1 = Conversation.where(user1: current_user)
+    as_user2 = Conversation.where(user2: current_user)
+    @contacts = as_user1 | as_user2
+
+    # Get the other profile in the conversation to access a profile picture
+    if @contacts.first.user1 == current_user
+      @profile = Profile.where(user: as_user1.first.user2).first
+    else
+      @profile = Profile.where(user: as_user1.first.user1).first
+    end
   end
 end
