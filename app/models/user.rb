d46cec3424af2861c423ea0529701ffb9c7b1647
NathanielMether <nmet3@student.monash.edu>
2017-11-09 16:05:28 +1100

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,10 +18,10 @@ class User < ApplicationRecord
   
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth[:info_fields]['email']
+      user.email = auth['info']['email']
       user.password = Devise.friendly_token[0,20]
-      user.first_name = auth[:info_fields]['first_name']
-      user.last_name = auth[:info_fields]['last_name']
+      user.first_name = auth['info']['first_name']
+      user.last_name = auth['info']['last_name']
     end
   end
 end
