8d6f117e8b27f71864658967f7a8ceb46e6d6c1c
NathanielMether <nmet3@student.monash.edu>
2017-11-21 13:44:42 +1100

    Made neater

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable, :timeoutable and :omniauthable
 
   devise :omniauthable, :omniauth_providers => [:facebook]
   validates :first_name, presence: true
